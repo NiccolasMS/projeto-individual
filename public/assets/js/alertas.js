32cc969292227fa16f83648377c8141f43a42a03
NiccolasMS <nicolas.santos@bandtec.com.br>
2021-11-23 23:53:36 -0300

    Adicionando site ao repositório do git

---
+++ public/assets/js/alertas.js
@@ -0,0 +1,59 @@
+function alertaSuccess(type, title, mensagem) {
+    Swal.fire({
+        icon: 'success',
+        type: type,
+        title: title,
+        text: mensagem,
+        showConfirmButton: false,
+        timer: 1500
+    });
+
+}
+function alertaError(type, title, mensagem) {
+    Swal.fire({
+        icon: 'error',
+        type: type,
+        title: title,
+        text: mensagem,
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
+function toastSuccess(title) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    Toast.fire({
+        icon: 'success',
+        title: title
+    })
+
+}
+function toastError(title) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    Toast.fire({
+        icon: 'error',
+        title: title
+    })
+
+}
